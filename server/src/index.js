4ffd8d855e47572537f72a72dd283998c4698e64
Abhishek Salokhe <abhisaloke2424@gmail.com>
2026-09-03 00:23:08 +0530

    Fix: Add missing fs import in server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const config = require('./config');
 const { FleetManager } = require('./simulator/fleet');
 const { FleetState } = require('./state');
